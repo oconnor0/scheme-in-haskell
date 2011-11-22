af3283bfff19843343c1c17004185a11ecfc3a47
Matthew O'Connor <thegreendragon@gmail.com>
2011-11-22 15:14:40 -0700

    added basic numeric eval

--- parse.hs
+++ parse.hs
@@ -86,13 +86,41 @@ parseExpr = parseAtom
                char ')'
                return x
 
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-  Left err -> "No match: " ++ show err
-  Right val -> "Found value: " ++ show val
+  Left err -> String $ "No match: " ++ show err
+  Right val -> val
+
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
+eval (List (Atom fn : args)) = apply fn $ map eval args
+
+apply :: String -> [LispVal] -> LispVal
+apply fn args = maybe (Bool False) ($ args) $ lookup fn primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n in
+                          if null parsed
+                            then 0
+                            else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
 
 main :: IO ()
-main = do
-  args <- getArgs
-  putStrLn $ args !! 0
-  putStrLn $ readExpr $ args !! 0
+main = getArgs >>= print . eval . readExpr . head
